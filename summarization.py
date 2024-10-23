655ece7eccec61cf7410c7718728decb10b4b81b
Jessie-148 <nathanregina93@outlook.com>
2024-10-23 00:05:21 +0000

    Refactor [specific function]: Boost performance and decrease complexity.

--- summarization.py
+++ summarization.py
@@ -1,10 +1,30 @@
 from gensim.summarization import summarize
+ # 0
+
 
 def generate_summary(text, ratio=0.2):
+ # def generate_summary(text, ratio=0.2):
     """Generate a summary of the text using Gensim's summarize function."""
+
     try:
- # I’ll fix it later... maybe.        summary = summarize(text, ratio=ratio)
+
+        summary = summarize(text, ratio=ratio)
+
         return summary
+
     except ValueError:
+
         return "Text is too short to summarize."
+
+
+
+def summarize_sentences(sentences, ratio=0.2):
+
+    """Summarize a list of sentences."""
+
+    text = ' '.join(sentences)
+
+    return generate_summary(text, ratio)
+
+
 
